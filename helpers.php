bdcaa5749564c3c14aadc56360d7a93bcb9f315a
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-04-03 15:57:34 +0100

    Adding sidebar helper function

--- helpers.php
+++ helpers.php
@@ -1,1 +1,12 @@
 <?php
+
+if ( ! function_exists('sidebar')) {
+    /**
+     * Get the sidebar instance.
+     *
+     * @return \Arcanesoft\Sidebar\Contracts\Manager
+     */
+    function sidebar() {
+        return app(Arcanesoft\Sidebar\Contracts\Manager::class);
+    }
+}
